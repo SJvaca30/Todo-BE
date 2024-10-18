1f1a85d11885c45fd2037581ca462512d9941e23
SJvaca30 <yyvaca30@gmail.com>
2024-10-19 00:54:48 +0900

    fix

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
 require('dotenv').config();
 const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,6 +19,6 @@ mongoose
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
